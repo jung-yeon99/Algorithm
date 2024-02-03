07b7b45202c8c38eb129fb46bde77b3495edeefb
JungyeonKim <83697577+jung-yeon99@users.noreply.github.com>
2024-02-03 15:24:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const N = Number(input[0]);
+let count = N;
+let word;
+
+for(let i=1; i <= N; i++) {
+    word = input[i];
+    for(let j=0; j < word.length-1; j++) {
+        if( word[j] != word[j+1] && !!word.slice(j+1).includes(word[j]) ) {
+            count--;
+            break;
+        }
+    }
+}
+console.log(count);
